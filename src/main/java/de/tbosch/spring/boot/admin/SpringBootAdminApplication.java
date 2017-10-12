9b6cb181edf2bdea4f6ce344fc88c7a07960278e
Thomas Bosch <tbosch@gmx.de>
2017-10-12 14:37:40 +0200

    moved class

--- src/main/java/de/tbosch/spring/boot/admin/SpringBootAdminApplication.java
+++ src/main/java/de/tbosch/spring/boot/admin/SpringBootAdminApplication.java
@@ -1,4 +1,4 @@
-package de.codecentric.boot.admin;
+package de.tbosch.spring.boot.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
