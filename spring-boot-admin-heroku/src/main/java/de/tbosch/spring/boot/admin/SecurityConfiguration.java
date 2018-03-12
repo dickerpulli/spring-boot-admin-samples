a2087fc58629b9e3906e3e8536248abaf9d17910
Thomas Bosch <tbosch@gmx.de>
2018-03-12 13:20:35 +0100

    Security config changed with Spring Boot 2.0

--- spring-boot-admin-heroku/src/main/java/de/tbosch/spring/boot/admin/SecurityConfiguration.java
+++ spring-boot-admin-heroku/src/main/java/de/tbosch/spring/boot/admin/SecurityConfiguration.java
@@ -9,33 +9,30 @@ import org.springframework.security.web.authentication.SavedRequestAwareAuthenti
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
 
 @Configuration
-public class SecurityConfiguration {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Configuration
-	public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-		private final String adminContextPath;
+	private final String adminContextPath;
 
-		public FormLoginWebSecurityConfigurerAdapter(AdminServerProperties adminServerProperties) {
-			this.adminContextPath = adminServerProperties.getContextPath();
-		}
+	public SecurityConfiguration(AdminServerProperties adminServerProperties) {
+		this.adminContextPath = adminServerProperties.getContextPath();
+	}
 
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
-			successHandler.setTargetUrlParameter("redirectTo");
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+		successHandler.setTargetUrlParameter("redirectTo");
 
-			http.authorizeRequests()//
-					.requestMatchers(EndpointRequest.to("info", "health")).permitAll()//
-					.requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR")//
-					.antMatchers(adminContextPath + "/assets/**").permitAll()//
-					.antMatchers(adminContextPath + "/login").permitAll()//
-					.anyRequest().authenticated()//
-					.and()//
-					.formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()//
-					.logout().logoutUrl(adminContextPath + "/logout").and()//
-					.httpBasic().and()//
-					.csrf().disable();//
-		}
+		http.authorizeRequests()//
+				.requestMatchers(EndpointRequest.to("info", "health")).permitAll()//
+				.requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR")//
+				.antMatchers(adminContextPath + "/assets/**").permitAll()//
+				.antMatchers(adminContextPath + "/login").permitAll()//
+				.anyRequest().authenticated()//
+				.and()//
+				.formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()//
+				.logout().logoutUrl(adminContextPath + "/logout").and()//
+				.httpBasic().and()//
+				.csrf().disable();//
 	}
 
 }
